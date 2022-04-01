63c22bc12e9e6aac7b8574d87016d20a8c6c1e61
shkarovskii <s9648785578@gmail.com>
2022-04-01 16:31:30 +0400

    @RequestParam, HttpServletRequest

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,18 +1,28 @@
 package ru.alishev.springcourse.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 	
 	@GetMapping("/hello")
-	public String sayHello() {
+	public String sayHello(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		
+		System.out.println("Hi " + name + " " + surname);
 		return "first/hello";
 	}
 	
 	@GetMapping("/goodbay")
-	public String sayGString() {
+	public String sayGString(@RequestParam(value = "name", required = false) String name,
+							@RequestParam(value = "surname", required = false) String surname) {
+		
+		System.out.println("Goodbay " + name + " " + surname);
 		return "first/goodbay";
 	}
 
