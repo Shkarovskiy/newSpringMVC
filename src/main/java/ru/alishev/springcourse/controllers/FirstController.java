cb7bd8583704120a87ebe2f073babe7ad3c2f6ff
shkarovskii <s9648785578@gmail.com>
2022-04-01 15:45:08 +0400

    first commit

---
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -0,0 +1,19 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "first/hello";
+	}
+	
+	@GetMapping("/goodbay")
+	public String sayGString() {
+		return "first/goodbay";
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>This is a start page. It's always is present when server is started</h2>
+</body>
+</html>
