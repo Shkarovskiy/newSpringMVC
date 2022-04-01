fcd2f05ed7d5c43a3da46b8c5bf1b3ce4ce61b70
shkarovskii <s9648785578@gmail.com>
2022-04-01 18:11:36 +0400

    Tyhmeleaf first touch

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,26 +3,34 @@ package ru.alishev.springcourse.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
+	private String messageKey = "message";
+	private String messageValue;
 	
 	@GetMapping("/hello")
-	public String sayHello(HttpServletRequest request) {
+	public String sayHello(HttpServletRequest request, Model model) {
 		String name = request.getParameter("name");
 		String surname = request.getParameter("surname");
 		
-		System.out.println("Hi " + name + " " + surname);
+		messageValue = "Hi my dear " + name + " " + surname;
+		model.addAttribute(messageKey, messageValue);
+		
 		return "first/hello";
 	}
 	
 	@GetMapping("/goodbay")
 	public String sayGString(@RequestParam(value = "name", required = false) String name,
-							@RequestParam(value = "surname", required = false) String surname) {
+							@RequestParam(value = "surname", required = false) String surname,
+							Model model) {
 		
-		System.out.println("Goodbay " + name + " " + surname);
+		messageValue = "Goodbay my dear" + name + " " + surname;
+		model.addAttribute(messageKey, messageValue);
+		
 		return "first/goodbay";
 	}
 
