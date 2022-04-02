1a014e0fb9461940f39686e8f2d0da9a7d8110e4
shkarovskii <s9648785578@gmail.com>
2022-04-02 08:17:36 +0400

    calculator

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -5,9 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 	private String messageKey = "message";
 	private String messageValue;
@@ -28,10 +30,41 @@ public class FirstController {
 							@RequestParam(value = "surname", required = false) String surname,
 							Model model) {
 		
-		messageValue = "Goodbay my dear" + name + " " + surname;
+		messageValue = "Goodbay my dear " + name + " " + surname;
 		model.addAttribute(messageKey, messageValue);
 		
 		return "first/goodbay";
+	}
+	
+	@GetMapping("/calculator")
+	public String calculate(@RequestParam(value = "a", required = false) Integer a,
+							@RequestParam(value = "b", required = false) Integer b,
+							@RequestParam(value = "operation", required = false) String operation,
+							Model model) {
+		
+		Integer result;
+		
+		if(operation != null) {
+			switch(operation) 
+		{
+		case "add":
+			result =  a + b;
+				break;
+		case "substract":
+			result = a - b;
+				break;
+		default:
+			result = -200;
+			}
+		} else {
+			result = -100;
+		}
+		System.out.println("debag: " + result);
+		
+		model.addAttribute("resultOfCalc", "Result of " + a + " " + operation + " " + b + " equals: " + result);
+//		model.addAttribute("resultOfCalc", "Result of ");
+				
+		return "first/calculator";
 	}
 
 }
