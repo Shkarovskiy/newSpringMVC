be53e5fe8b0a8efc4b3b67b6971e604586c30d45
Sergei Shkarovskii <s9648785578@gmail.com>
2022-04-04 01:35:46 +0400

    Create, Update

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ru.alishev.springcourse.dao.PersonDAO;
+import ru.alishev.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +35,51 @@ public class PeopleController {
 		model.addAttribute("person", personDAO.show(id));
 		return "people/show";
 	}
+	
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("person") Person person) {
+		return "people/new";
+	}
+	
+	@PostMapping
+	public String create(@ModelAttribute("person") Person person) {
+		personDAO.save(person);
+		return "redirect:/people";
+	}
+	
+	@GetMapping("/{id}/edit")
+	public String edit(Model model, @PathVariable("id") int id) {
+		model.addAttribute("person", personDAO.show(id));
+		return "people/edit";
+	}
+	
+	@PatchMapping("/{id}")
+	public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+		personDAO.update(id, person);
+		return "redirect:/people";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
